c20bd89780217572dcb83d58cdb28deb493edbf2
Ashna Shah-Grover <as19@williams.edu>
2020-01-13 00:44:07 -0500

    Less complex solution to stars question

--- practiceForPythonInterview/starsQuestion.rb
+++ practiceForPythonInterview/starsQuestion.rb
@@ -7,6 +7,25 @@ def closest_stars(stars, k)
   return stars.to_h.first(k).to_h
 end
 
+def closest_stars(stars, k)
+  shortest = stars.first(k).to_h
+  shortest = shortest.sort_by { |name, x_y| (x_y[0]**2+x_y[1]**2)**0.5 }.to_h
+  n = stars.keys.length
+  stars__={}
+  stars.keys[k..n-1].each { |key| stars__[key]=stars[key] }
+  stars__.each do |name, x_y|
+    x = shortest.values.last[0]
+    y = shortest.values.last[1]
+    max = (x**2 + y**2)**0.5
+    current_star_dist = (x_y[0]**2+x_y[1]**2)**0.5
+    if current_star_dist < max
+      shortest.delete(shortest.keys.last)
+      shortest[name] = x_y
+      shortest = shortest.sort_by { |name, x_y| (x_y[0]**2+x_y[1]**2)**0.5 }.to_h
+    end
+  end
+  return shortest
+end
 
 
 stars = {
